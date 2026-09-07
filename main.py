0638fc07b88d1d6c88b6ec55082fc353b6dba538
mim2014cc-spec <mim2014cc@gmail.com>
2026-09-07 11:01:32 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# name = input("Enter you name")
+# print("Hello world.." + name)
+# print("I misss you" , name)
+
+# year = input("Enter you year of birth.")
+# age = 2026 - int(year)
+# print("You are" + str(age) +  " yaer old.")
+
+# for i in range(0,10):
+#     print ( str(i))
+
